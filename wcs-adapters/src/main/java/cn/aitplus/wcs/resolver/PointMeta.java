fc34da9ffe8c7b7fa89e618b2052b02ffd1ed103
guanyunpeng <guanyunpeng@163.com>
2026-03-23 14:15:12 +0800

    删除没用的

--- wcs-adapters/src/main/java/cn/aitplus/wcs/resolver/PointMeta.java
+++
@@ -1,7 +0,0 @@
-package cn.aitplus.wcs.resolver;
-
-public class PointMeta {
-    public final String address;
-    public final String dataType;
-    public PointMeta(String addr, String dt){this.address=addr;this.dataType=dt;}
-}
--- wcs-infra/src/main/java/cn/aitplus/wcs/infra/service/task/WareHouseModeService.java
+++
@@ -1,8 +0,0 @@
-package cn.aitplus.wcs.infra.service.task;
-
-public interface WareHouseModeService {
-
-    String getCurrentMode(Integer warehouseId);
-
-    String changeMode(Integer warehouseId, int mode);
-}
